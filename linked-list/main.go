f0eace3d5a17b7ccb9091bf3000f0a6a2ebdd09c
lyx0 <stefan.parfuss@protonmail.ch>
2021-03-29 20:37:38 +0200

    add error handling

--- linked-list/main.go
+++ linked-list/main.go
@@ -30,8 +30,21 @@ func (l linkedList) printListData() {
 }
 
 func (l *linkedList) deleteWithValue(value int) {
+	if l.length == 0 {
+		return
+	}
+
+	if l.head.data == value {
+		l.head = l.head.next
+		l.length--
+		return
+	}
+
 	previousToDelete := l.head
 	for previousToDelete.next.data != value {
+		if previousToDelete.next.next == nil {
+			return
+		}
 		previousToDelete = previousToDelete.next
 	}
 	previousToDelete.next = previousToDelete.next.next
