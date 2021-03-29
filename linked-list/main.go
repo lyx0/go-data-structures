ad44fb72ef18bcd14f4e10f8ed48a23c22835db3
lyx0 <stefan.parfuss@protonmail.ch>
2021-03-29 20:25:16 +0200

    add printing of linked list

--- linked-list/main.go
+++ linked-list/main.go
@@ -8,24 +8,41 @@ type node struct {
 }
 
 type linkedList struct {
-	head *node
+	head   *node
 	length int
 }
 
-func (l *linkedList) prepend (n *node) {
+func (l *linkedList) prepend(n *node) {
 	second := l.head
 	l.head = n
 	l.head.next = second
 	l.length++
 }
 
+func (l linkedList) printListData() {
+	toPrint := l.head
+	for l.length != 0 {
+		fmt.Printf("%d ", toPrint.data)
+		toPrint = toPrint.next
+		l.length--
+	}
+	fmt.Printf("\n")
+}
+
 func main() {
 	myList := linkedList{}
-	node1 := &node{data:48}
-	node2 := &node{data:123}
-	node3 := &node{data:12}
+	node1 := &node{data: 48}
+	node2 := &node{data: 123}
+	node3 := &node{data: 1231}
+	node4 := &node{data: 134}
+	node5 := &node{data: 15343}
+	node6 := &node{data: 14122}
 	myList.prepend(node1)
 	myList.prepend(node2)
 	myList.prepend(node3)
-	fmt.Println(myList)
+	myList.prepend(node4)
+	myList.prepend(node5)
+	myList.prepend(node6)
+	// fmt.Println(myList)
+	myList.printListData()
 }
